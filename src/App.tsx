35795a6bd7b66f70bb661228d2ca2196a53f59f5
ytulupov <y@tulupov.org>
2026-01-28 15:17:33 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Привет мир</h1>
+    </div>
+  )
+}
+
+export default App
